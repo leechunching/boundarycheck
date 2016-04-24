e3cfe7ed7fe8e4ff45be4d7170d362401d08b9c9
Scott.Lee <scott.lee@quantatw.com>
2016-04-24 11:07:07 +0800

    Modify auto run script to run different algorithm. -mtype <num>. where num, 1, MI 2, ISD-FSM 3, AID 4, MI+JE (todo)

--- autorunmovie.sh
+++ autorunmovie.sh
@@ -2,5 +2,5 @@ MOVIE_PATH=/home/dream/Android_Lowpower_project/Movie/480_360_4/
 for movie_num in 2
 do
 
-  ./ffplay -autoexit -skip_frame none ${MOVIE_PATH}${movie_num}.mp4
+  ./ffplay -mtype 1 -autoexit -skip_frame none ${MOVIE_PATH}${movie_num}.mp4
 done
